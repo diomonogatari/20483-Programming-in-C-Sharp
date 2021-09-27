73ba93f1d51311e2aea174ac6da4d07d96911c36
Diomonogatari <diodude.lol@gmail.com>
2021-09-27 15:18:44 +0100

    Module 1 Exercise 1

--- Allfiles/Mod01/Labfiles/Starter/Exercise 1/School/MainWindow.xaml.cs
+++ Allfiles/Mod01/Labfiles/Starter/Exercise 1/School/MainWindow.xaml.cs
@@ -8,13 +8,11 @@ using System.Windows.Input;
 using School.Data;
 using System.Globalization;
 
-namespace School
-{
+namespace School {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
+    public partial class MainWindow : Window {
         // Connection to the School database
         private SchoolDBEntities schoolContext = null;
 
@@ -26,21 +24,18 @@ namespace School
 
         #region Predefined code
 
-        public MainWindow()
-        {
+        public MainWindow() {
             InitializeComponent();
         }
 
         // Connect to the database and display the list of teachers when the window appears
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
             this.schoolContext = new SchoolDBEntities();
             teachersList.DataContext = this.schoolContext.Teachers;
         }
 
         // When the user selects a different teacher, fetch and display the students for that teacher
-        private void teachersList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+        private void teachersList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
             // Find the teacher that has been selected
             this.teacher = teachersList.SelectedItem as Teacher;
             this.schoolContext.LoadProperty<Teacher>(this.teacher, s => s.Students);
@@ -55,46 +50,67 @@ namespace School
         #endregion
 
         // When the user presses a key, determine whether to add a new student to a class, remove a student from a class, or modify the details of a student
-        private void studentsList_KeyDown(object sender, KeyEventArgs e)
-        {
+        private void studentsList_KeyDown(object sender, KeyEventArgs e) {
             // TODO: Exercise 1: Task 1a: If the user pressed Enter, edit the details for the currently selected student
-            // TODO: Exercise 1: Task 2a: Use the StudentsForm to display and edit the details of the student
-            // TODO: Exercise 1: Task 2b: Set the title of the form and populate the fields on the form with the details of the student
-            // TODO: Exercise 1: Task 3a: Display the form
-            // TODO: Exercise 1: Task 3b: When the user closes the form, copy the details back to the student
-            // TODO: Exercise 1: Task 3c: Enable saving (changes are not made permanent until they are written back to the database)
+
+            switch (e.Key) {
+                case Key.Enter:
+                    //from the selected item, parse as a object from our school model
+                    Student selectedStudent = ((ListView)sender).SelectedItem as Student;
+
+                    //Exercise 1: Task 2a:
+                    StudentForm s = new StudentForm();
+                    s.firstName.Text = selectedStudent.FirstName;
+                    s.lastName.Text = selectedStudent.LastName;
+                    s.dateOfBirth.Text = selectedStudent.DateOfBirth.ToString("yyyy-MM-dd");
+
+                    //Exercise 1: Task 2b:
+                    s.Title = "Edit Student record";
+
+                    //Exercise 1: Task 3a:
+                    //if the form that is open returns true from the Ok button then a change was made
+                    if (s.ShowDialog().Value) {
+
+                        //Exercise 1: Task 3b:
+                        //On closing with success save the data
+                        selectedStudent.FirstName = s.firstName.Text;
+                        selectedStudent.LastName = s.lastName.Text;
+                        selectedStudent.DateOfBirth = DateTime.ParseExact(s.dateOfBirth.Text, "yyyy-MM-dd",CultureInfo.InvariantCulture);
+
+                        //Exercise 1: Task 3c:
+                        //because a change was made, the save button is now enabled
+                        saveChanges.IsEnabled = true;
+                    }
+                    break;
+            }
         }
 
         #region Predefined code
 
-        private void studentsList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
- 
+        private void studentsList_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+
         }
 
         // Save changes back to the database and make them permanent
-        private void saveChanges_Click(object sender, RoutedEventArgs e)
-        {
-
+        private void saveChanges_Click(object sender, RoutedEventArgs e) {
+            schoolContext.SaveChanges();
+            saveChanges.IsEnabled = false;
         }
 
         #endregion
     }
 
     [ValueConversion(typeof(string), typeof(Decimal))]
-    class AgeConverter : IValueConverter
-    {
+    class AgeConverter : IValueConverter {
         public object Convert(object value, Type targetType, object parameter,
-                              System.Globalization.CultureInfo culture)
-        {
+                              System.Globalization.CultureInfo culture) {
             return "";
         }
 
         #region Predefined code
 
         public object ConvertBack(object value, Type targetType, object parameter,
-                                  System.Globalization.CultureInfo culture)
-        {
+                                  System.Globalization.CultureInfo culture) {
             throw new NotImplementedException();
         }
 
